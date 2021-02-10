a1503041845787310b0881220a8a68118d40995a
Rees Draminski <rees.draminski@gmail.com>
2021-02-10 14:15:46 -0500

    add plus/minus buttons for num inputs

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
-// Constants that can be changed every year
+// constants that can be changed every year
 const UMBC_TUITION = 4352;
 const SEMESTER_LENGTH = 16;
 const COST_PER_CREDIT = 361;
 
+const tuitionInfoEl = document.getElementById("info");
+const numCreditsInput = document.getElementById("numCredits");
+
 let totalMoneyLost = 0;
 
-// update information disclosure with up to date stuff
-let info = document.getElementById("info");
-info.innerHTML = info.innerHTML.replace("{{SEMESTER_LENGTH}}", SEMESTER_LENGTH);
-info.innerHTML = info.innerHTML.replace("{{COST_PER_CREDIT}}", COST_PER_CREDIT);
+(function initUI() {
+    // update information disclosure with up to date stuff
+    tuitionInfoEl.innerHTML = tuitionInfoEl.innerHTML.replace("{{SEMESTER_LENGTH}}", SEMESTER_LENGTH);
+    tuitionInfoEl.innerHTML = tuitionInfoEl.innerHTML.replace("{{COST_PER_CREDIT}}", COST_PER_CREDIT);
 
-// Update the money lost value whenever something is changed
-document.getElementById("numCredits").addEventListener("input", calculateLoss);
-document.getElementById("numClassMeetings").addEventListener("input", calculateLoss);
-document.getElementById("numSkips").addEventListener("input", calculateLoss);
+    // update the money lost value whenever something is changed
+    document.getElementById("numCredits").addEventListener("input", calculateLoss);
+    document.getElementById("numClassMeetings").addEventListener("input", calculateLoss);
+    document.getElementById("numSkips").addEventListener("input", calculateLoss);
+
+    // go through each number input div and add the plus/minus buttons
+    [...document.querySelectorAll(".numberInput")]
+        .forEach(el => {
+            // the number input div should only ever have two children
+            const [ input, buttonGroup ] = el.children;
+
+            // create an add to number input button
+            createElement(buttonGroup, "span", {
+                class: "fa fa-plus",
+                style: "color: green; font-size: 1.5em",
+                onclick: () => {
+                    // get the value from the input element and add one to it
+                    input.value = parseInt(input.value, 10) + 1;
+
+                    // re-calculate tuition lost figure
+                    calculateLoss();
+                }
+            });
+
+            // create a subtract from number input button
+            createElement(buttonGroup, "span", {
+                class: "fa fa-minus",
+                style: "color: red; font-size: 1.5em; margin-left: 10px",
+                onclick: () => {
+                    // get the value from the input element
+                    const val = parseInt(input.value, 10);
+
+                    // do not allow negatives
+                    if (val !== 0)
+                    {
+                        // get the value from the numCredits input and subtract one from it
+                        input.value = val - 1;
+
+                        // re-calculate tuition lost figure
+                        calculateLoss();
+                    }
+                }
+            });
+        });   
+})();
 
 function addToRunningTotal() {
     // get input values
@@ -101,4 +145,51 @@ function calculateLoss() {
     if (!isNaN(moneyLost) && moneyLost >= 0 && moneyLost <= totalClassValue) {
         document.getElementById("tuitionLost").innerHTML = moneyLost.toLocaleString(undefined, { style: 'currency',minimumFractionDigits: 2, maximumFractionDigits: 2, currency: 'USD' });
     }
+}
+
+/**
+ * Create an HTML element and add it to the DOM tree.
+ * @param {HTMLElement} parent 
+ * @param {String} tag 
+ * @param {Object} attributes 
+ */
+function createElement(parent, tag, attributes={}) {
+    // create the element to whatever tag was given
+    const el = document.createElement(tag);
+    
+    // go through all the attributes in the object that was given
+    Object.entries(attributes)
+        .forEach(([attr, value]) => {
+            // handle the various special cases that will cause the Element to be malformed
+            if (attr == "innerText") 
+            {
+                el.innerText = value;
+            }
+            else if (attr == "innerHTML") 
+            {
+                el.innerHTML = value;
+            }
+            else if (attr == "textContent") 
+            {
+                el.textContent = value;
+            }
+            else if (attr == "onclick")
+            {
+                el.onclick = value;
+            }
+            else if (attr == "onkeydown")
+            {
+                el.onkeydown = value;
+            }
+            else
+            {
+                el.setAttribute(attr, value);
+            }
+        });
+    
+    // add the newly created element to its parent
+    parent.appendChild(el);
+
+    // return the element in case this element is a parent for later element creation
+    return el;
 }
